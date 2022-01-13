eee0db35539a7a7789b601ea886f4298024eb0e6
Harshit Saxena <hsaxena@microsoft.com>
2022-01-13 22:57:13 +0530

    add config file

--- provider/dataset-azure/src/main/java/org/opengroup/osdu/dataset/provider/azure/service/DatasetDmsServiceMapImpl.java
+++ provider/dataset-azure/src/main/java/org/opengroup/osdu/dataset/provider/azure/service/DatasetDmsServiceMapImpl.java
@@ -17,9 +17,10 @@
 package org.opengroup.osdu.dataset.provider.azure.service;
 
 import org.opengroup.osdu.dataset.dms.DmsServiceProperties;
+import org.opengroup.osdu.dataset.provider.azure.config.OsduApiConfig;
+import org.opengroup.osdu.dataset.provider.azure.config.OsduDatasetKindConfig;
 import org.opengroup.osdu.dataset.provider.interfaces.IDatasetDmsServiceMap;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.PropertySource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
@@ -27,31 +28,24 @@ import java.util.HashMap;
 import java.util.Map;
 
 @Service
-@PropertySource("classpath:osdu-dataset.properties")
 public class DatasetDmsServiceMapImpl implements IDatasetDmsServiceMap {
 
     private final Map<String, DmsServiceProperties> resourceTypeToDmsServiceMap = new HashMap<>();
 
-    @Value("${osdu.kind.file}")
-    private String fileKind;
+    @Autowired
+    OsduApiConfig osduApiConfig;
 
-    @Value("${osdu.kind.file.collection}")
-    private String fileCollectionKind;
-
-    @Value("${osdu.api.endpoints.file}")
-    private String fileApi;
-
-    @Value("${osdu.api.endpoints.file.collection}")
-    private String fileCollectionApi;
+    @Autowired
+    OsduDatasetKindConfig osduDatasetKindConfig;
 
     @PostConstruct
     public void init() {
-        DmsServiceProperties fileDmsProperties = new DmsServiceProperties(fileApi);
+        DmsServiceProperties fileDmsProperties = new DmsServiceProperties(osduApiConfig.getFile());
         fileDmsProperties.setStagingLocationSupported(true);
 
         //TODO: replace this with static or dynamic registration of DMS
-        resourceTypeToDmsServiceMap.put(fileKind, fileDmsProperties);
-        resourceTypeToDmsServiceMap.put(fileCollectionKind, getDmsServicePropertyForFileCollection());
+        resourceTypeToDmsServiceMap.put(osduDatasetKindConfig.getFile(), fileDmsProperties);
+        resourceTypeToDmsServiceMap.put(osduDatasetKindConfig.getFileCollection(), getDmsServicePropertyForFileCollection());
     }
 
     @Override
@@ -60,7 +54,7 @@ public class DatasetDmsServiceMapImpl implements IDatasetDmsServiceMap {
     }
 
     private DmsServiceProperties getDmsServicePropertyForFileCollection() {
-        DmsServiceProperties fileCollectionDmsProperties = new DmsServiceProperties(fileCollectionApi);
+        DmsServiceProperties fileCollectionDmsProperties = new DmsServiceProperties(osduApiConfig.getFileCollection());
         fileCollectionDmsProperties.setStagingLocationSupported(true);
         return fileCollectionDmsProperties;
     }
