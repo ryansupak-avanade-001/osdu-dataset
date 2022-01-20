b317b2d9bb4d1cd0eb639cffe38e54de9f3c5b9c
Yifei Xu <yifeix@amazon.com>
2022-01-20 23:47:06 +0000

    Temporarily revert the change to use dummy cache when in local mode

--- provider/dataset-aws/src/main/java/org/opengroup/osdu/dataset/provider/aws/cache/DmsRegistrationCache.java
+++ provider/dataset-aws/src/main/java/org/opengroup/osdu/dataset/provider/aws/cache/DmsRegistrationCache.java
@@ -14,13 +14,6 @@
 
 package org.opengroup.osdu.dataset.provider.aws.cache;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import org.opengroup.osdu.core.aws.cache.DummyCache;
-import org.opengroup.osdu.core.aws.ssm.K8sLocalParameterProvider;
-import org.opengroup.osdu.core.aws.ssm.K8sParameterNotFoundException;
-import org.opengroup.osdu.core.common.cache.ICache;
-import org.opengroup.osdu.core.common.cache.VmCache;
-import org.opengroup.osdu.core.common.model.entitlements.Groups;
 import org.opengroup.osdu.core.common.model.http.DpsHeaders;
 
 import org.opengroup.osdu.core.common.cache.RedisCache;
@@ -29,37 +22,10 @@ import org.opengroup.osdu.dataset.provider.aws.model.DmsRegistrations;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-import java.util.Map;
-
 @Component
-public class DmsRegistrationCache<K, V> implements ICache<K, V> {
-    @Value("${aws.elasticache.cluster.endpoint:null}")
-    String REDIS_SEARCH_HOST;
-    @Value("${aws.elasticache.cluster.port:null}")
-    String REDIS_SEARCH_PORT;
-    @Value("${aws.elasticache.cluster.key:null}")
-    String REDIS_SEARCH_KEY;
-    private ICache cache;
-
-    public DmsRegistrationCache() throws K8sParameterNotFoundException, JsonProcessingException {
-        K8sLocalParameterProvider provider = new K8sLocalParameterProvider();
-        if (provider.getLocalMode()) {
-            if (Boolean.parseBoolean(System.getenv("DISABLE_CACHE"))) {
-                this.cache = new DummyCache();
-            }
-            this.cache = new VmCache<>(60, 10);
-        } else {
-            String host = provider.getParameterAsStringOrDefault("CACHE_CLUSTER_ENDPOINT", REDIS_SEARCH_HOST);
-            int port = Integer.parseInt(provider.getParameterAsStringOrDefault("CACHE_CLUSTER_PORT", REDIS_SEARCH_PORT));
-            Map<String, String> credential = provider.getCredentialsAsMap("CACHE_CLUSTER_KEY");
-            String password;
-            if (credential != null) {
-                password = credential.get("token");
-            } else {
-                password = REDIS_SEARCH_KEY;
-            }
-            this.cache = new RedisCache(host, port, password, 60, String.class, Groups.class);
-        }
+public class DmsRegistrationCache extends RedisCache<String, DmsRegistrations> {
+    public DmsRegistrationCache(@Value("${aws.elasticache.cluster.endpoint}") final String REDIS_HOST, @Value("${aws.elasticache.cluster.port}") final String REDIS_PORT, @Value("${aws.elasticache.cluster.key}") final String REDIS_KEY) {
+        super(REDIS_HOST, Integer.parseInt(REDIS_PORT), REDIS_KEY, 300, String.class, DmsRegistrations.class);
     }
 
     public static String getCacheKey(DpsHeaders headers) {
@@ -68,23 +34,4 @@ public class DmsRegistrationCache<K, V> implements ICache<K, V> {
         return Crc32c.hashToBase64EncodedString(key);
     }
 
-    @Override
-    public void put(K k, V o) {
-        this.cache.put(k, o);
-    }
-
-    @Override
-    public V get(K k) {
-        return (V) this.cache.get(k);
-    }
-
-    @Override
-    public void delete(K k) {
-        this.cache.delete(k);
-    }
-
-    @Override
-    public void clearAll() {
-        this.cache.clearAll();
-    }
 }
